b3b78995dc4402cf78058f3bfed31fdbbabb022d
Hans <a0903150698@gmail.com>
2024-03-23 18:59:06 +0800

    first commit

--- FengChiaActivities/ViewController.swift
+++ FengChiaActivities/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        registerButton.layer.cornerRadius = 10.0
+        loginButton.layer.cornerRadius = 10.0
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
